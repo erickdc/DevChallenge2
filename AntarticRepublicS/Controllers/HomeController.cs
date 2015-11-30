186164daf94262fb06977516e283da47c1c619fb
erickdc <erickson_523@hotmail.com>
2015-11-30 12:36:08 -0600

    fixes

--- AntarticRepublicS/Controllers/HomeController.cs
+++ AntarticRepublicS/Controllers/HomeController.cs
@@ -16,56 +16,57 @@ namespace AntarticRepublicS.Controllers
 {
     public class HomeController : Controller
     {
+        private static string _secretMessage = " ";
         [HttpPost]
         public ActionResult Index(SecretModel model)
         {
-            
-           
-            ViewBag.phrase = model.Secret;
-            return View();
+
+
+            _secretMessage = model.Secret;
+            return RedirectToAction("Index");
         }
 
         public ActionResult Encrypt()
         {
 
-            var listWords = new List<string>();
-            int amountTimes = 20;
-            var listFibonacciSequenceModel = new List<FibonacciSequenceModel>();
-          //  var listFibonacciSequenceModelCopy = new List<FibonacciSequenceModel>();
-            var responseList = new List<ResponseStatusModel>();
-            for (int i = 0; i < amountTimes; i++)
-            {
-                var guid = Guid.NewGuid();
-                listFibonacciSequenceModel.Add(
-                    MapJsonToModel<FibonacciSequenceModel>("http://internal-devchallenge-2-dev.apphb.com/values/" + guid));
-               // var temp = new FibonacciSequenceModel();
-            //    temp.Words = listFibonacciSequenceModel[i].Words.Clone() as string[];
-           //     listFibonacciSequenceModelCopy.Add(temp);
-                    IEncrypter encrpyter = SantaEncrypter.GetCorrespondingEncrypter(listFibonacciSequenceModel[i].Algorithm,
-                    listFibonacciSequenceModel[i]);
-                listWords.Add(encrpyter.Encrypt());
-                if (listWords.Count > i)
-                {
-                    PayLoadRequestModel request = new PayLoadRequestModel()
-                    {
-                        EncodedValue = listWords[i],
-                        EmailAddress = "erickdcb10@gmail.com",
-                        Name = "Erick Caballero",
-                        RepoUrl = "https://github.com/erickdc/DevChallenge2",
-                        WebHookUrl = "http://antarticchallengedev2.apphb.com/Home/Index/"
+              var listWords = new List<string>();
+              int amountTimes = 20;
+              var listFibonacciSequenceModel = new List<FibonacciSequenceModel>();
+            //  var listFibonacciSequenceModelCopy = new List<FibonacciSequenceModel>();
+              var responseList = new List<ResponseStatusModel>();
+              for (int i = 0; i < amountTimes; i++)
+              {
+                  var guid = Guid.NewGuid();
+                  listFibonacciSequenceModel.Add(
+                      MapJsonToModel<FibonacciSequenceModel>("http://internal-devchallenge-2-dev.apphb.com/values/" + guid));
+                 // var temp = new FibonacciSequenceModel();
+              //    temp.Words = listFibonacciSequenceModel[i].Words.Clone() as string[];
+             //     listFibonacciSequenceModelCopy.Add(temp);
+                      IEncrypter encrpyter = SantaEncrypter.GetCorrespondingEncrypter(listFibonacciSequenceModel[i].Algorithm,
+                      listFibonacciSequenceModel[i]);
+                  listWords.Add(encrpyter.Encrypt());
+                  if (listWords.Count > i)
+                  {
+                      PayLoadRequestModel request = new PayLoadRequestModel()
+                      {
+                          EncodedValue = listWords[i],
+                          EmailAddress = "erickdcb10@gmail.com",
+                          Name = "Erick Caballero",
+                          RepoUrl = "https://github.com/erickdc/DevChallenge2",
+                          WebHookUrl ="http://antarticchallengedev2.apphb.com/Home/Index/"
 
-                    };
-                    ResponseStatusModel response =
-                        PostPayLoad<ResponseStatusModel>("http://internal-devchallenge-2-dev.apphb.com", request, guid, listFibonacciSequenceModel[i].Algorithm);
-                    responseList.Add(response); 
-                }
-            }
+                      };
+                      ResponseStatusModel response =
+                          PostPayLoad<ResponseStatusModel>("http://internal-devchallenge-2-dev.apphb.com", request, guid, listFibonacciSequenceModel[i].Algorithm);
+                      responseList.Add(response); 
+                  }
+              }
             return RedirectToAction("Index");
         }
         // GET: Home
         public ActionResult Index()
         {
-        
+            ViewBag.phrase = _secretMessage;
             return View();
         }
         public T MapJsonToModel<T>(string url)
